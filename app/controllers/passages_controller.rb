74410ec4d78df993349dd13eb826653a7e89d589
Brad Olson <brado@movedbylight.com>
2013-10-17 16:13:14 -0400

    GREEN-specs work (butrequire API access).

--- app/controllers/passages_controller.rb
+++ app/controllers/passages_controller.rb
@@ -2,7 +2,7 @@ class PassagesController < ApplicationController
 	
 	def show
 		@passages = API.passages(params[:q], {:versions=>params[:version_uid]})
-		@copyrights = @passages.collect(&:copyright).uniq
+		@copyrights = @passages.collection.collect(&:copyright).uniq
 	end
 
 end
--- spec/features/chapters_spec.rb
+++ spec/features/chapters_spec.rb
@@ -15,7 +15,7 @@ describe "view chapter" do
 
 	it "displays book name and chapter" do
 		visit "/eng-CEV/Gen/1" 
-		page.should have_xpath("chapter_title", :text => "Genesis 1")
+		page.should have_selector("#chapter_title", :text => /Genesis\s+1/)
 	end
 
 end
