d61967e80afaf7edee85a02a05aa855262cd9f0c
Brad Olson <brado@movedbylight.com>
2013-10-18 11:54:36 -0400

    GREEN-fixed the test

--- app/models/api.rb
+++ app/models/api.rb
@@ -1,12 +1,12 @@
 class API
   extend ActiveModel::Naming
-  
+
   def initialize(attributes = {})
     attributes.each do |name, value|
       send("#{name}=", value)
     end
   end
-  
+
   def persisted?
     false
   end
--- spec/models/api_spec.rb
+++ spec/models/api_spec.rb
@@ -4,9 +4,9 @@ require 'spec_helper'
 describe API do
 
   it %{includes chapters when fetching books} do
-    genesis = API.books('eng-CEV').collection[0]
+    genesis = API.books_with_chapters('eng-CEV').collection[0]
     # puts genesis
-    genesis.keys.should include("chapters")
+    genesis.chapters.collection.should have(50).chapters
   end
 
 end
